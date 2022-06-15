cb9bda5088b53b452f6fe5d0fcfc4a153228e645
haorenzhi <horange1024@outlook.com>
2022-06-15 02:13:22 -0500

    Django Model Classification Parsing Workflow done

---
+++ classification/management/commands/parse_gspy_classifications.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+from cmath import e
+from ctypes import sizeof
+from django.core.management.base import BaseCommand, CommandError
+import panoptes_client
+
+from classification.models import Classification
+
+class Command(BaseCommand):
+    help = 'Querying the Gravity Spy Plus zooniverse project for classifications'
+    def add_arguments(self, parser):
+        parser.add_argument("--project-id", default='9979')
+        parser.add_argument("--last-classification-id", type=int, default=None)
+        parser.add_argument("--verbose", type=bool, default=True)
+    #last_id reference?
+    def handle(self, *args, **options):
+        if options['last_classification_id'] is not None:
+            all_classifications = panoptes_client.Classification.where(project_id=options['project_id'], scope='project', last_id='{0}'.format(options['last_classification_id']), page_size='100')
+        else:
+            all_classifications = panoptes_client.Classification.where(project_id=options['project_id'], scope='project', page_size='100')
+
+        list_of_classification_dictionaries = []
+        # Loop until no more classifications
+        for iN in range(0,all_classifications.object_count):
+            try:
+                classification = all_classifications.next()
+                list_of_classification_dictionaries.append(classification.raw)
+            except:
+                break
+
+        for classification in list_of_classification_dictionaries:
+            if classification['links']['workflow'] == '21793':
+                classification_id=classification['id']
+                #Create an annotation dictionary
+                annotation_counts = [0] * 3
+                for i in range(0, 3):
+                    annotation_counts[i] = 0
+                #Get the annotations labels based on relative y coordinate
+                annotation_list = classification['annotations'][0]['value']
+                height = 600 #height of a spectrum in pixel
+                for j in range(len(annotation_list)):
+                    y = annotation_list[j]['y']
+                    if y >= height and y < 2 * height:
+                        annotation_counts[0] = 1
+                    elif y >= 2 * height and y <= 3 * height:
+                        annotation_counts[1] = 1
+                    else:
+                        annotation_counts[2] = 1
+                #Save all the annotation labels in the list
+                annotation = []
+                for k in range(len(annotation_counts)):
+                    if annotation_counts[k] == 1:
+                        annotation.append(k)
+
+                workflow_id=classification['links']['workflow']
+                user_id=classification['links']['user']
+                subject_id=classification['links']['subjects'][0]      
+                #Log on the terminal
+                if options['verbose'] is True:
+                    print("id is {0}".format(classification_id))
+                    print("annotation is {0}".format(annotation))
+                    print("workflow is {0}".format(workflow_id))
+                    print("user is {0}".format(user_id))
+                    print("subject is {0}".format(subject_id))
+
+                #Instantiate the classification object
+                classification_subject = Classification.objects.create_classification(classification_id=classification_id, annotation=annotation, workflow_id=workflow_id,user_id=user_id, subject_id=subject_id)
+                
+                #Save out the subject
+                classification_subject.save()
---
+++ classification/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+#Customize the model by custom manager
+class ClassificationManager(models.Manager):
+    #Create the classification subject
+    def create_classification(self, classification_id, annotation, workflow_id, user_id, subject_id):
+        self.classification_id=classification_id
+        self.annotation=annotation
+        self.workflow_id=workflow_id
+        self.user_id=user_id
+        self.subject_id=subject_id
+        classification_subject = self.create(classification_id=self.classification_id, annotation=self.annotation, workflow_id=self.workflow_id, user_id=self.user_id, subject_id=self.subject_id)
+        return classification_subject
+
+#Create the Django model
+class Classification(models.Model):
+    """The frame work for a single Gravity Spy annotation/classification from a volunteer
+    id is 420780262
+    annotation is [{'task': 'T0', 'value': [{'x': 401.2159729003906, 'y': 1486.4444580078125, 'tool': 0, 'frame': 0, 'details': []}]}]
+    workflow is 21793
+    user is 386563
+    subject is 76084855    
+    """
+
+    #Field defining
+    classification_id = models.IntegerField()
+    annotation = ArrayField(models.IntegerField())
+    workflow_id = models.IntegerField()
+    user_id = models.IntegerField()
+    subject_id = models.IntegerField()
+    ##project_id?
+
+    #Add a method on custom manager
+    objects = ClassificationManager()
+
